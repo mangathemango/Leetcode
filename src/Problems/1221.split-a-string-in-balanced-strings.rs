314cc5b6d19ec8277283035d11d1a5c1db476e53
mangathemango <163281872+mangathemango@users.noreply.github.com>
2025-04-05 06:50:46 +0800

    [1221] Add Copilot suggestion

--- src/Problems/1221.split-a-string-in-balanced-strings.rs
+++ src/Problems/1221.split-a-string-in-balanced-strings.rs
@@ -8,19 +8,15 @@
 impl Solution {
     pub fn balanced_string_split(s: String) -> i32 {
         let mut count = 0;
-        let mut l_count: u16 = 0;
-        let mut r_count = 0;
+        let mut balance: u16 = 0;
         for ch in s.chars() {
-            if ch == 'R' {
-                r_count += 1;
+            match ch {
+                'R' => balance += 1,
+                'L' => balance -= 1,
+                _ => ()
             }
-            if ch == 'L' {
-                l_count += 1;
-            }
-            if r_count == l_count {
+            if balance == 0 {
                 count += 1;
-                r_count = 0;
-                l_count = 0;
             }
         }
         count
