81ec142f6ff5881a4bb47e5c39ed3fffd7b97190
mangathemango <163281872+mangathemango@users.noreply.github.com>
2025-04-05 14:40:14 +0800

    [2682] Use bool vec instead of int vec

--- src/Problems/2682.find-the-losers-of-the-circular-game.rs
+++ src/Problems/2682.find-the-losers-of-the-circular-game.rs
@@ -7,19 +7,29 @@
 // @lc code=start
 impl Solution {
     pub fn circular_game_losers(n: i32, k: i32) -> Vec<i32> {
-        let mut ball_passed = Vec::<i32>::new();
-        let mut ball = 0;
+        let mut ball_passed = vec![false; n as usize];
+        let mut ball: i32 = 0;
         let mut step = k;
+
         loop {
-            if ball_passed.contains(&(ball + 1)) {
-                break;
+            if ball_passed[ball as usize] {
+                break
             }
-            ball_passed.push(ball + 1);
+            ball_passed[ball as usize] = true;
             ball = (ball + step) % n;
-            step += k;
+            step += k
         }
 
-        (1..=n).filter(|x| !ball_passed.contains(&x)).collect::<Vec<i32>>()
+        let mut losers = Vec::<i32>::new();
+        for (i, passed) in ball_passed.iter().enumerate() {
+            if !*passed {
+                losers.push((i + 1) as i32)
+            }
+        }
+
+        losers
+
+
     }
 }
 // @lc code=end
