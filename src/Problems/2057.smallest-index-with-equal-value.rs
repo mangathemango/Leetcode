f2fec436c75486c745c064463ccf64a0ed521e60
mangathemango <163281872+mangathemango@users.noreply.github.com>
2025-04-05 14:52:04 +0800

    [2057] Second solution with .find_map()

--- src/Problems/2057.smallest-index-with-equal-value.rs
+++ src/Problems/2057.smallest-index-with-equal-value.rs
@@ -7,12 +7,17 @@
 // @lc code=start
 impl Solution {
     pub fn smallest_equal(nums: Vec<i32>) -> i32 {
-        for (i, num) in nums.iter().enumerate() {
-            if (i as i32) % 10 == *num {
-                return i as i32;
-            }
-        }
-        -1
+        nums
+            .iter()
+            .enumerate()
+            .find_map(
+                |(i, num)| 
+                if (i % 10) as i32 == *num 
+                    {Some(i as i32)} 
+                else 
+                    {None}
+            )
+            .unwrap_or(-1)
     }
 }
 // @lc code=end
